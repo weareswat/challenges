ffa56fe5b55ca0c827b44292fdcf3d92006d40f7
Marco de Freitas <marcogfreitas@gmail.com>
2020-07-01 21:18:35 +0100

    Add initial scaffold

---
+++ 1-development/ListPostsByRating/src/ListPostsByRating.Api/Controllers/PostsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPut("upvote/{postId}")]
+        public void Upvote(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpPut("downvote/{postId}")]
+        public void Downvote(int id, [FromBody] string value)
+        {
+        }
+    }
+}
