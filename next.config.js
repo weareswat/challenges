76e9e1065069dedfe79891614e3c1eb1c1707999
Diego Melo <diegomelo182@gmail.com>
2021-10-10 02:29:38 -0300

    feat: deploy changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  PUBLIC_URL: 'https://diegomelo182.github.io/invoicexpress-react-test',
+  assetPrefix: `./`,
   reactStrictMode: true,
   env: {
     API_HOST: process.env.API_HOST,
---
+++ replacer.js
@@ -0,0 +1,15 @@
+const replace = require("replace-in-file");
+const options = {
+  //you may need to modify the file address to suite your project
+  files: "./build/index.html",
+  from: [/src="\//g, /href="\//g],
+  to: ['src="./', 'href="./'],
+};
+(async function () {
+  try {
+    const results = await replace(options);
+    console.log("Replacement results:", results);
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+})();
