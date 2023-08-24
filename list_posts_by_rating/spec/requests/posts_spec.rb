9821cc40745cd8b6c0518e542667a71735d0d119
Pedro Pinheiro <ppinheiro@avidgaming.com>
2023-08-24 17:15:22 -0300

    Clean up, add code coverage, improve tests

--- list_posts_by_rating/spec/requests/posts_spec.rb
+++ list_posts_by_rating/spec/requests/posts_spec.rb
@@ -14,24 +14,46 @@ RSpec.describe '/posts', type: :request do
   describe 'PUT /upvote' do
     let!(:my_post) { Fabricate :post, upvotes: 0, downvotes: 0 }
 
-    it 'increments post upvotes by 1' do
-      put post_upvote_url(my_post), as: :json
-      my_post.reload
-      expect(my_post.upvotes).to eq 1
-      expect(my_post.downvotes).to eq 0
-      expect(response).to be_successful
+    context 'success' do
+      it 'increments post upvotes by 1' do
+        put post_upvote_url(my_post), as: :json
+        my_post.reload
+        expect(my_post.upvotes).to eq 1
+        expect(my_post.downvotes).to eq 0
+        expect(response).to be_successful
+      end
+    end
+
+    context 'internal server error' do
+      before { allow_any_instance_of(Post).to receive(:increment_upvotes).and_return(nil) }
+
+      it 'returns a 500 error' do
+        put post_upvote_url(my_post), as: :json
+        expect(response).to have_http_status(500)
+      end
     end
   end
 
   describe 'PUT /downvote' do
     let!(:my_post) { Fabricate :post, upvotes: 0, downvotes: 0 }
 
-    it 'increments post downvotes by 1' do
-      put post_downvote_url(my_post), as: :json
-      my_post.reload
-      expect(my_post.upvotes).to eq 0
-      expect(my_post.downvotes).to eq 1
-      expect(response).to be_successful
+    context 'success' do
+      it 'increments post downvotes by 1' do
+        put post_downvote_url(my_post), as: :json
+        my_post.reload
+        expect(my_post.upvotes).to eq 0
+        expect(my_post.downvotes).to eq 1
+        expect(response).to be_successful
+      end
+    end
+
+    context 'internal server error' do
+      before { allow_any_instance_of(Post).to receive(:increment_downvotes).and_return(nil) }
+
+      it 'returns a 500 error' do
+        put post_downvote_url(my_post), as: :json
+        expect(response).to have_http_status(500)
+      end
     end
   end
 end
--- list_posts_by_rating/spec/routing/posts_routing_spec.rb
+++ list_posts_by_rating/spec/routing/posts_routing_spec.rb
@@ -6,25 +6,12 @@ RSpec.describe PostsController, type: :routing do
       expect(get: "/posts").to route_to("posts#index")
     end
 
-    it "routes to #show" do
-      expect(get: "/posts/1").to route_to("posts#show", id: "1")
+    it "routes to #upvote" do
+      expect(put: "/upvote/1").to route_to("posts#upvote", id: "1")
     end
 
-
-    it "routes to #create" do
-      expect(post: "/posts").to route_to("posts#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(put: "/posts/1").to route_to("posts#update", id: "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(patch: "/posts/1").to route_to("posts#update", id: "1")
-    end
-
-    it "routes to #destroy" do
-      expect(delete: "/posts/1").to route_to("posts#destroy", id: "1")
+    it "routes to #downvote" do
+      expect(put: "/downvote/1").to route_to("posts#downvote", id: "1")
     end
   end
 end
