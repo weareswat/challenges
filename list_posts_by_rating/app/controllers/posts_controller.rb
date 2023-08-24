29c6a818a2cdb5ea17c75c0838b6d2ff832b3457
Pedro Pinheiro <ppinheiro@avidgaming.com>
2023-08-24 16:34:25 -0300

    Clean up

--- list_posts_by_rating/app/controllers/posts_controller.rb
+++ list_posts_by_rating/app/controllers/posts_controller.rb
@@ -27,7 +27,6 @@ class PostsController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
   end
