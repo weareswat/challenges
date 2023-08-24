f9216f770b8dd4bfb4dc9d287dc4c718f3635b94
Pedro Pinheiro <ppinheiro@avidgaming.com>
2023-08-24 16:01:07 -0300

    Upvote action implementation

--- list_posts_by_rating/app/controllers/posts_controller.rb
+++ list_posts_by_rating/app/controllers/posts_controller.rb
@@ -1,51 +1,25 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show update destroy ]
+  before_action :set_post, only: %i[upvote downvote]
 
   # GET /posts
   def index
-    @posts = Post.all
+    @posts = Post.all_posts_ranked_desc
 
     render json: @posts
   end
 
-  # GET /posts/1
-  def show
-    render json: @post
-  end
-
-  # POST /posts
-  def create
-    @post = Post.new(post_params)
-
-    if @post.save
-      render json: @post, status: :created, location: @post
-    else
-      render json: @post.errors, status: :unprocessable_entity
-    end
-  end
-
   # PATCH/PUT /posts/1
-  def update
-    if @post.update(post_params)
-      render json: @post
+  def upvote
+    if @post.increment_upvotes
+      render status: :ok
     else
-      render json: @post.errors, status: :unprocessable_entity
+      render json: @post.errors, status: 500
     end
-  end
-
-  # DELETE /posts/1
-  def destroy
-    @post.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:upvotes, :downvotes, :content)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
