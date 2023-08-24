11918dc99b4bc145fd8486cbfc14354a1066f02e
Pedro Pinheiro <ppinheiro@avidgaming.com>
2023-08-24 02:39:56 -0300

    Tests for manipulating upvotes/downvotes

--- list_posts_by_rating/spec/models/post_spec.rb
+++ list_posts_by_rating/spec/models/post_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:post) { Fabricate :post }
+
+  describe '#increment_upvotes' do
+    it 'Increments upvotes by 1' do
+      post.increment_upvotes
+
+      expect(post.reload.upvotes).to eq 2
+    end
+  end
+
+  describe '#increment_downvotes' do
+    it 'Increments downvotes by 1' do
+      post.increment_downvotes
+
+      expect(post.reload.downvotes).to eq 2
+    end
+  end
+
+  describe '#engagement' do
+    it 'returns upvotes + downvotes' do
+      expect(post.engagement).to eq 2
+    end
+  end
 end
