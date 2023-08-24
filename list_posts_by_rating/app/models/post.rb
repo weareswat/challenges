3b71677a59cbdcdfe081881fda256c7e5f8631d5
Pedro Pinheiro <ppinheiro@avidgaming.com>
2023-08-24 02:40:27 -0300

    Implement methods for manipulating upvotes/downvotes

--- list_posts_by_rating/app/models/post.rb
+++ list_posts_by_rating/app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  def increment_upvotes
+    ActiveRecord::Base.transaction do
+      update(upvotes: upvotes + 1)
+    end
+  end
+
+  def increment_downvotes
+    ActiveRecord::Base.transaction do
+      update(downvotes: downvotes + 1)
+    end
+  end
+
+  def engagement
+    upvotes + downvotes
+  end
 end
