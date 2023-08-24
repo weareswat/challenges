70f2252d91bc3e78f0c5c7b8022a440ada27abd5
GabrielVieiraDw <gabrielvieiradw@gmail.com>
2023-08-24 17:46:31 -0300

    Add spec for model post

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+  end
+end
