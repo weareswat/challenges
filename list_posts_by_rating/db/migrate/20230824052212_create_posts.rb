cf02b3f06510f9f55e344de3048d1a8c877bb1e4
Pedro Pinheiro <ppinheiro@avidgaming.com>
2023-08-24 03:07:36 -0300

    Set upvote/downvote defaults

--- list_posts_by_rating/db/migrate/20230824052212_create_posts.rb
+++ list_posts_by_rating/db/migrate/20230824052212_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.integer :upvotes
-      t.integer :downvotes
+      t.integer :upvotes, default: 0
+      t.integer :downvotes, default: 0
       t.string :content
 
       t.timestamps
