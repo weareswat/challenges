353ef3f9d482a3572ba1a88b356f5b25de10025a
Pedro Pinheiro <ppinheiro@avidgaming.com>
2023-08-24 19:39:18 -0300

    Avoid race conditions

--- list_posts_by_rating/app/models/post.rb
+++ list_posts_by_rating/app/models/post.rb
@@ -1,12 +1,12 @@
 class Post < ApplicationRecord
   def increment_upvotes
-    ActiveRecord::Base.transaction do
+    with_lock do
       update(upvotes: upvotes + 1)
     end
   end
 
   def increment_downvotes
-    ActiveRecord::Base.transaction do
+    with_lock do
       update(downvotes: downvotes + 1)
     end
   end
