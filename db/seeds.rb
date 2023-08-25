0a94a35ebb46899b1d9e13d1da9f210dda76557a
GabrielVieiraDw <gabrielvieiradw@gmail.com>
2023-08-24 21:55:10 -0300

    Create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'factory_bot_rails'
+
+puts "-- Seeding Posts"
+
+FactoryBot.create_list(:post, 10)
+
+puts "-- Seeding Users"
+
+FactoryBot.create_list(:user, 10)
