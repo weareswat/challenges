7c83104800a529b54cbd17302c8ec04cd097868f
Paulo <paultergust@gmail.com>
2023-08-31 19:36:13 -0300

    Add route to mock data

--- posts/config/routes.rb
+++ posts/config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "mock#create_mocks"
   get '/users', to: 'users#index',:defaults => { :format => 'json' }
   post '/users', to: 'users#create',:defaults => { :format => 'json' }
   get '/posts', to: 'posts#index', :defaults => { :format => 'json' }
+  post '/posts', to: 'posts#create',:defaults => { :format => 'json' }
   get '/upvote/:id', to: 'posts#upvote', :defaults => { :format => 'json' }
   get '/downvote/:id', to: 'posts#downvote', :defaults => { :format => 'json' }
 end
