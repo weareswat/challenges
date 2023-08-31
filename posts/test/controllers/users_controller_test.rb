bdce90fb6df8290cb5d8a4debbdfa6bc3f2b4b3e
Paulo <paultergust@gmail.com>
2023-08-31 15:24:11 -0300

    Add tests for user controller

--- posts/test/controllers/users_controller_test.rb
+++ posts/test/controllers/users_controller_test.rb
@@ -4,4 +4,23 @@ class UserControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should create user" do
+    testcase_username = "bob_or_alice"
+    data = {username: testcase_username}
+    post "/users", params: data
+    assert_response :success
+
+    user = User.first
+    assert_equal testcase_username, user.username
+  end
+
+  test "should list all users" do
+    six = 6
+    six.times do |i|
+      User.create(username: i.to_s)
+    end
+    get "/users"
+    assert_response :success
+    assert_equal six, JSON.parse(@response.body).length
+  end
 end
