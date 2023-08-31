538d6eb6c371ab4c641c4143393834871d3e7fa2
Paulo <paultergust@gmail.com>
2023-08-31 19:35:02 -0300

    Set project as API only

--- posts/config/application.rb
+++ posts/config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Posts
   class Application < Rails::Application
+    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
