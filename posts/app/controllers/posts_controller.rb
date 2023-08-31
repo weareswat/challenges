eaa1a7c7b59dc4bb05c48eec6d89ea83f9b06e49
Paulo <paultergust@gmail.com>
2023-08-31 15:00:05 -0300

    Fix upvote/downvote endpoints

--- posts/app/controllers/posts_controller.rb
+++ posts/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
 
   def create
     user = User.find(username: params[:username])
@@ -20,13 +21,21 @@ class PostsController < ApplicationController
   def upvote
     @post = Post.find(params[:id])
     @post.upvotes += 1
-    render nothing: true, status: 200
+    if @post.save!
+      respond_to do |format|
+        format.json { render json: @post }
+      end
+    end
   end
 
   def downvote
     @post = Post.find(params[:id])
     @post.downvotes += 1
-    render nothing: true, status: 200
+    if @post.save!
+      respond_to do |format|
+        format.json { render json: @post }
+      end
+    end
   end
 
   private
