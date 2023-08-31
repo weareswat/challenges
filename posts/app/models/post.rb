983550948b7c8669661ff3cac9175c35e256e0ed
Paulo <paultergust@gmail.com>
2023-08-31 02:29:21 -0300

    Add methods to calculate ratio and sort posts

--- posts/app/models/post.rb
+++ posts/app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  def ratio
+    total = self.upvotes + self.downvotes
+    (self.upvotes.to_f / total.to_f * 100).round(2)
+  end
+
+  def self.sorted(query = nil)
+    posts = query.nil? ? Post.all : where(query)
+
+    posts.sort {|a,b| a.ratio == b.ratio ? b.upvotes <=> a.upvotes : b.ratio <=> a.ratio}
+  end
 end
