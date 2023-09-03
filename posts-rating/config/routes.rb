109c087c06164f259c8c37937fe8b7a9a93c4b43
Guilherme Pereira <guilhermewop@gmail.com>
2023-09-03 14:27:03 -0300

    defining routes as needs

--- posts-rating/config/routes.rb
+++ posts-rating/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
-  resources :votes
+  # resources :votes, only: [:create] do
+  #   member do
+  #     post :increase_up_vote, path: 'upvote'
+  #     post :increase_down_vote, path: 'downvote'
+  #   end
+  # end
+
   resources :posts
+  post '/upvote/:post_id', to: 'votes#create', defaults: { vote_type: :up }, as: 'post_up_vote'
+  post '/downvote/:post_id', to: 'votes#create', defaults: { vote_type: :down }, as: 'post_down_vote'
 end
