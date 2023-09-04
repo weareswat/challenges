1fde5dfe35969607bdeaef4c94ac64921dea99c2
Guilherme Pereira <guilhermewop@gmail.com>
2023-09-04 12:06:21 -0300

    refactoring permitted parameters

--- posts-rating/app/controllers/votes_controller.rb
+++ posts-rating/app/controllers/votes_controller.rb
@@ -4,7 +4,7 @@ class VotesController < ApplicationController
   # POST /upvote/1
   # POST /downvote/1
   def create
-    if @post.rate(Vote.new(vote_params.merge(vote_type: params[:vote_type])))
+    if @post.rate(Vote.new(vote_params))
       render json: @post, status: :created, location: @post
     else
       render json: @post.errors, status: :unprocessable_entity
@@ -17,6 +17,7 @@ class VotesController < ApplicationController
     end
 
     def vote_params
-      params.require(:vote).permit(:post_id, :vote_type, :username)
+      params.require(:username)
+      params.permit(:username, :vote_type)
     end
 end
