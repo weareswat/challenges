e2d740fdc85b57618d8ec1e3b25aab955c6e9d3f
Guilherme Pereira <guilhermewop@gmail.com>
2023-09-04 01:15:36 -0300

    more verbose post payload

--- posts-rating/app/serializers/post_serializer.rb
+++ posts-rating/app/serializers/post_serializer.rb
@@ -1,11 +1,15 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :up_votes, :down_votes, :score
+  attributes :id, :title, :content, :votes
 
-  def up_votes
-    object.up_votes_count
-  end
+  def votes
+    up_votes   = object.up_votes_count
+    down_votes = object.down_votes_count
 
-  def down_votes
-    object.down_votes_count
+    {
+      score: object.score,
+      total: up_votes + down_votes,
+      up: up_votes,
+      down: down_votes,
+    }
   end
 end
