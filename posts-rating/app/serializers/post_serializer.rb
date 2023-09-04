374573b3602502c80e47532b7404d20c7f5437a3
Guilherme Pereira <guilhermewop@gmail.com>
2023-09-03 22:48:39 -0300

    added more attributes to post paylod

--- posts-rating/app/serializers/post_serializer.rb
+++ posts-rating/app/serializers/post_serializer.rb
@@ -1,3 +1,11 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content
+  attributes :id, :title, :content, :up_votes, :down_votes, :score
+
+  def up_votes
+    object.up_votes_total
+  end
+
+  def down_votes
+    object.down_votes_total
+  end
 end
