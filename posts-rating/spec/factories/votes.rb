662b6cf1ea81f44bef0a1900d9b163aa40145852
Guilherme Pereira <guilhermewop@gmail.com>
2023-09-04 04:45:59 -0300

    just make tests more random

--- posts-rating/spec/factories/votes.rb
+++ posts-rating/spec/factories/votes.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     username { Faker::Internet.username(specifier: 6..12, separators: ['.']) }
 
     transient do
-      vote_type { :up }
+      vote_type { [:up, :down].sample }
     end
 
     after(:build) do |vote, evaluator|
