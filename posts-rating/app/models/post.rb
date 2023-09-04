86170554fe5a961bc46b442ca92a6e434c9b11e2
Guilherme Pereira <guilhermewop@gmail.com>
2023-09-03 22:47:39 -0300

    developing post model class

--- posts-rating/app/models/post.rb
+++ posts-rating/app/models/post.rb
@@ -1,5 +1,34 @@
 class Post < ApplicationRecord
-  has_many :post
+  has_many :votes, foreign_key: 'post_id', class_name: 'Vote'
 
   validates :title, :content, presence: true
+
+  scope :ordered_by_score, -> { sort_by { |p| -p.score } }
+
+  def increase_vote(vote)
+    self.votes << vote
+
+    save!
+  end
+
+  def up_votes_total
+    count_votes(vote_type: :up)
+  end
+
+  def down_votes_total
+    count_votes(vote_type: :down)
+  end
+
+  def score
+    up_votes   = self.up_votes_total
+    down_votes = self.down_votes_total
+
+    (up_votes.to_f / down_votes.to_f) * (up_votes + down_votes)
+  end
+
+  private
+
+  def count_votes(vote_type:)
+    self.votes.where(vote_type: vote_type).count
+  end
 end
