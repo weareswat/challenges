d87ea70067369c8944ca82ad1faec85a135e9674
Guilherme Pereira <guilhermewop@gmail.com>
2023-09-04 01:15:52 -0300

    added some db seeds

--- posts-rating/db/seeds.rb
+++ posts-rating/db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+def generate_data_for_post
+  {
+    title: Faker::Lorem.words(number: rand(2..6)).join(' '),
+    content: Faker::Lorem.paragraphs(number: rand(2..5)).join(' ')
+  }
+end
+
+post_one = Post.create(generate_data_for_post)
+post_two = Post.create(generate_data_for_post)
+
+60.times do
+  post_one.increase_vote(Vote.new(username: 'guilherme', vote_type: :up))
+end
+
+40.times do
+  post_one.increase_vote(Vote.new(username: 'guilherme', vote_type: :down))
+end
+
+6.times do
+  post_two.increase_vote(Vote.new(username: 'guilherme', vote_type: :up))
+end
+
+4.times do
+  post_two.increase_vote(Vote.new(username: 'guilherme', vote_type: :down))
+end
